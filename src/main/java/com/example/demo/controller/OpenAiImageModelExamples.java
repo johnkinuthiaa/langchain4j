948b04ab0ed95914255cdd48f7f27605b8b5a6b5
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-09-28 13:51:27 +0300

    initial commit

---
+++ src/main/java/com/example/demo/controller/OpenAiImageModelExamples.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.*;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+
+@RestController
+@RequestMapping("/home")
+public class OpenAiImageModelExamples {
+    @GetMapping("/img")
+    public String getJoke(@RequestParam String message){
+        ChatLanguageModel model = OpenAiChatModel.builder()
+                .apiKey("input your api key")
+                .modelName(GPT_4_O_MINI)
+                .build();
+        return model.generate(message);
+    }
+
+}
